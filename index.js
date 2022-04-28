078403ee4d2045377f0f62d9598419c313fa5fbf
owkmear <h3s8qp9xw3f@gmail.com>
2022-04-28 18:15:51 +0300

    Remove example

--- index.js
+++ index.js
@@ -126,32 +126,3 @@ function getTestDirectories() {
     .map((dirent) => dirent.name)
     .filter((name) => name !== ".git");
 }
-
-const question = {
-  id: "54b6bbd5-55a6-4b09-a497-7584ec5b1966",
-  grade: "Middle",
-  theme: "Тонкости и неоднозначности языка",
-  question: `Что НЕ является валидным?`,
-  code: `const bird = {
-  size: "small"
-};
-
-const mouse = {
-  name: "Mickey",
-  small: true
-};`,
-  correctAnswer: 1,
-  variants: [
-    `mouse.bird.size`,
-    `mouse[bird.size]`,
-    `mouse[bird["size"]]`,
-    `Все варианты валидны`,
-  ],
-  explanation: `В JavaScript все ключи объекта являются строками (кроме Symbol). И хотя мы не набираем их как строки, они всегда преобразовываются к строкам под капотом.
-
-JavaScript интерпретирует (или распаковывает) операторы. При использовании квадратных скобок JS замечает [ и продолжает пока не встретит ]. Только после этого он вычислит то, что находится внутри скобок.
-
-mouse[bird.size]: Сперва определяется bird.size, которое равно "small". mouse["small"] возвращает true.
-
-Но с записью через точку так не происходит. У mouse нет ключа bird. Таким образом, mouse.bird равно undefined. Затем мы запрашиваем ключ size, используя точечную нотацию: mouse.bird.size. Так как mouse.bird это undefined, мы запрашиваем undefined.size. Это не является валидным, и мы получаем ошибку типа Cannot read property "size" of undefined.`,
-};
